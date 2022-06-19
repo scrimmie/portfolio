90dada3e621ddf4ecd76e0386a4ae0a16067bde5
scrimmie <drdapice@comcast.net>
2022-06-19 19:26:59 -0400

    adding keyframe animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,17 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ['./src/**/*.svelte', './src/**/*.css'],
   media: false,
-  theme: {},
+  theme: {
+    animation: {
+      wiggle: 'wiggle 1s ease-in-out infinite',
+    },
+    keyframes: {
+      wiggle: {
+        '0%, 100%': { transform: 'rotate(-3deg)' },
+        '50%': { transform: 'rotate(3deg)' },
+      }
+    }
+  },
   variants: {
     extend: {},
   },
