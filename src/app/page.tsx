c480df023a03330b82f4ed026217543cee77b600
scrimmie <drdapice@comcast.net>
2023-03-26 13:12:10 -0400

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline text-red-600 font-JetbrainsMono">Hello world! test</h1>
+      <h1 className="text-3xl font-bold underline text-red-600 font-JetbrainsMono">Hello world! test2</h1>
     </main>
   );
 }
