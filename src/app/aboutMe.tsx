dcb7eef5f5e8ea68d10122a53bf6f4bd98b562a7
scrimmie <drdapice@comcast.net>
2023-09-04 13:18:18 -0400

    updating about me to include new image and adding text

--- src/app/aboutMe.tsx
+++ src/app/aboutMe.tsx
@@ -1,27 +1,35 @@
-import BrowserWindow from "../components/browserWindow";
-import Image from "next/image";
+import BrowserWindow from '../components/browserWindow';
+import Image from 'next/image';
 
 export default function AboutMe() {
   return (
-    <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-transparent">
-      <div className="container w-screen h-screen flex flex-col items-center justify-center p-10 py-16">
-        <BrowserWindow title="aboutMe.com">
-          <div className="w-full h-full flex flex-row items-center justify-center">
-            <div className="p-10 w-full h-full flex flex-row items-center justify-center">
-              <Image
-                src="/davidDapice.JPG"
-                alt="Picture of the author"
-                width={380}
-                height={380}
-              />
+    <div className='sticky top-0 flex h-screen flex-col items-center justify-center bg-transparent'>
+      <div className='container flex h-screen w-screen flex-col items-center justify-center p-10 py-16'>
+        <BrowserWindow title='aboutMe.com'>
+          <div className='relative z-0 flex h-full w-full flex-row items-center justify-center bg-white'>
+            <div className='relative flex h-full w-full flex-row items-center justify-center p-10'>
+              <Image src='/davidDapice.JPG' alt='Picture of the author' fill />
+              <div className='absolute bottom-1.5 left-1.5 flex flex-col transition duration-700 ease-in-out hover:scale-105'>
+                <div className='font-JetbrainsMono text-9xl text-white'>
+                  David
+                </div>
+                <div className='font-JetbrainsMono text-9xl text-white'>
+                  D&apos;Apice
+                </div>
+              </div>
             </div>
-            <p className="p-10 h-full w-full font-JetbrainsMono text-white text-2xl whitespace-pre-wrap flex flex-col items-center justify-center">   I am a driven, passionate technologist who combines logic and
+          </div>
+          <div className='relative z-10 flex h-full w-full flex-row items-center justify-center bg-red-500'>
+            <p className='flex h-full w-full flex-col items-center justify-center whitespace-pre-wrap p-10 font-JetbrainsMono text-2xl text-black'>
+              I am a driven, passionate technologist who combines logic and
               discipline with interpersonal skills that power new thinking.
-              <br/> <br/>  As far back as I can remember, I&apos;ve had a fascination with how
-              things work. My natural curiosity has helped me learn and discover
-              <br/><br/>   As a team player, I thrive on collaboration, and have an innate interest in
-              the world around me — especially when technology can impact the
-              lives of others.
+              <br /> <br /> As far back as I can remember, I&apos;ve had a
+              fascination with how things work. My natural curiosity has helped
+              me learn and discover
+              <br />
+              <br /> As a team player, I thrive on collaboration, and have an
+              innate interest in the world around me — especially when
+              technology can impact the lives of others.
             </p>
           </div>
         </BrowserWindow>
--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -3,7 +3,7 @@ import BrowserWindow from "../components/browserWindow";
 export default function Projects() {
     return (
         <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-transparent">
-        <div className="container w-screen h-screen flex flex-col items-center justify-center p-10 py-1">
+        <div className="container w-screen h-screen flex flex-col items-center justify-center p-10 py-16">
         <BrowserWindow title="projects.me">
         <div>TEST</div>
         </BrowserWindow>
